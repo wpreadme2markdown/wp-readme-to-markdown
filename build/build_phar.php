0761a0a10a8fd42addb76f80d7124f1da67b5ca1
Christian Archer <chrstnarchr@aol.com>
2014-05-18 20:59:39 +0400

    fix phar being not directly executable in unix

--- build/build_phar.php
+++ build/build_phar.php
@@ -11,7 +11,10 @@ if (is_file(__DIR__ . '/wp2md.phar')) {
 }
 
 // init phar archive
-$phar = new Phar(__DIR__ . '/wp2md.phar');
+$phar_file = __DIR__ . '/wp2md.phar';
+$phar = new Phar($phar_file);
+
+$phar->startBuffering();
 
 // run cli
 $stub_file = file_get_contents(__DIR__.'/../bin/wp2md.php');
@@ -19,7 +22,19 @@ $version   = trim(`git describe --tags HEAD`);
 $stub_file = preg_replace('/@package_version@/', $version, $stub_file);
 
 $phar->addFromString('bin/wp2md.php', $stub_file);
-$phar->setDefaultStub('bin/wp2md.php');
 
 // add files
 $phar->buildFromDirectory(__DIR__ . '/../','@/(src|vendor)/@');
+
+// Get the default stub. You can create your own if you have specific needs
+$defaultStub = $phar->createDefaultStub('bin/wp2md.php');
+
+// Create a custom stub to add the shebang
+$stub = "#!/usr/bin/env php \n".$defaultStub;
+// Add the stub
+$phar->setStub($stub);
+
+$phar->stopBuffering();
+
+$phar_file = escapeshellarg($phar_file);
+print `chmod a+x $phar_file`;
