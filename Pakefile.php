8f0f5755fc1b044988b955689871dff51fba815b
Christian Archer <sunchaser@sunchaser.info>
2016-08-12 02:52:50 +0300

    use new phar compiler

--- Pakefile.php
+++ Pakefile.php
@@ -2,40 +2,71 @@
 pake_desc('Build phar archive');
 pake_task('phar');
 
+function exec_composer($path)
+{
+    $prev_dir = getcwd();
+    chdir($path);
+
+    $composer = `which composer`;
+
+    if (empty($composer)) {
+        // official composer installation guide from https://getcomposer.org/download/
+        copy('https://getcomposer.org/installer', 'composer-setup.php');
+        if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') {
+            echo 'Installer verified';
+        } else {
+            echo 'Installer corrupt';
+        }
+        echo PHP_EOL;
+        pake_sh('/usr/bin/env php composer-setup.php');
+        unlink('composer-setup.php');
+
+        $composer = 'php composer.phar';
+    }
+
+    $composer = trim($composer);
+
+    pake_sh("$composer install --no-dev -o");
+
+    chdir($prev_dir);
+}
+
 function run_phar()
 {
     $build_dir = __DIR__ . '/build';
 
-    pake_echo_action('phar', 'create/clean build dir');
+    pake_echo_action('phar', 'prepare build dir');
 
     pake_mkdirs($build_dir);
     pake_remove(pakeFinder::type('file')->name('*'), $build_dir);
 
+    // project files
+    pake_mirror(pakeFinder::type('file')->name('*.php'), __DIR__ . '/bin', $build_dir . '/bin');
+    pake_mirror(pakeFinder::type('file')->name('*.php'), __DIR__ . '/src', $build_dir . '/src');
+
+    // make clean library installation without dev dependencies
+    pake_copy(__DIR__ . '/composer.json', $build_dir . '/composer.json');
+    pake_copy(__DIR__ . '/composer.lock', $build_dir . '/composer.lock');
+    exec_composer($build_dir);
+
+    pake_echo_action('phar', 'set product version');
+
+    $bin_file   = file_get_contents($build_dir . '/bin/wp2md.php');
+    $version    = trim(pake_sh('git describe --tags HEAD'));
+    $bin_file   = preg_replace('/@package_version@/', $version, $bin_file);
+    file_put_contents($build_dir . '/bin/wp2md.php', $bin_file);
+
     pake_echo_action('phar', 'init phar archive');
-    $phar_file = $build_dir . '/wp2md.phar';
-    $phar = new Phar($phar_file);
 
-    $phar->startBuffering();
+    $phar = new \Secondtruth\Compiler\Compiler($build_dir);
 
-    pake_echo_action('phar', 'determine and set version');
-    $stub_file = file_get_contents(__DIR__ . '/bin/wp2md.php');
-    $version   = trim(pake_sh('git describe --tags HEAD'));
-    $stub_file = preg_replace('/@package_version@/', $version, $stub_file);
+    $phar->addDirectory('bin');
+    $phar->addDirectory('src');
+    $phar->addDirectory('vendor', array('!*.php', '*Test.php', '*Tester.php', '*/Tests/*'));
 
-    $phar->addFromString('bin/wp2md.php', $stub_file);
+    $phar->addIndexFile('bin/wp2md.php', 'cli');
 
-    pake_echo_action('phar', 'add files');
-    $phar->buildFromDirectory(__DIR__ . '/','@/(src|vendor)/@');
-
-    pake_echo_action('phar', 'make stub executable');
-    // Get the default stub
-    $defaultStub = $phar->createDefaultStub('bin/wp2md.php');
-    // Create a custom stub to add the shebang
-    $stub = "#!/usr/bin/env php \n".$defaultStub;
-    // Add the stub
-    $phar->setStub($stub);
-
-    $phar->stopBuffering();
+    $phar->compile($build_dir . '/wp2md.phar');
 
     pake_chmod('wp2md.phar', $build_dir, 0755);
 
--- bin/wp2md.php
+++ bin/wp2md.php
@@ -20,5 +20,3 @@ if (is_file(__DIR__ . '/../vendor/autoload.php')) {
 $application = new \Symfony\Component\Console\Application('WPReadme2Markdown', '@package_version@');
 $application->add(new \WPReadme2Markdown\Cli\Convert());
 $application->run();
-
-__HALT_COMPILER(); // allow project to be used in PHAR
